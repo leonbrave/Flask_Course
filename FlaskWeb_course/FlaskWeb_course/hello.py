c3c86c060b7c0ad3e63adbd4037c5d30fc1e6f1e
leonbrave <savega@uc.cl>
2015-09-30 15:50:39 -0300

    Model Definitions

--- FlaskWeb_course/FlaskWeb_course/hello.py
+++ FlaskWeb_course/FlaskWeb_course/hello.py
@@ -234,5 +234,23 @@ db = SQLAlchemy(app)    #The db object instantiated from class SQLAlchemy repres
                         #access to all the functionality of Flask-SQLAlchemy
 
 
+
+#{::::::::::::::::::::::::::::::    Example 5-2. hello.py: Role and User model definition
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+   
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+     return '<User %r>' % self.username
+
 if __name__ == '__main__':
     app.run(debug=True)
